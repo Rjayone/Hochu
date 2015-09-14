538021cd1a1667510fef8f7208472607e15229d2
Andrew <admin@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2015-09-14 20:40:35 +0300

    Initial Commit

---
+++ Hochu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hochu
+//
+//  Created by Andew Medvedev on 14.09.15.
+//  Copyright (c) 2015 Andew Medvedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hochu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hochu
+//
+//  Created by Andew Medvedev on 14.09.15.
+//  Copyright (c) 2015 Andew Medvedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
